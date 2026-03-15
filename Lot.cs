6b332fb608abcfabbfbbf9e5e05744a9de1e02b3
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-03-15 14:39:02 -0400

    Add project files.

---
+++ Lot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_Lot
+{
+    class ParkingSystem
+    {
+        int big;
+        int medium;
+        int small;
+
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        ParkingSystem p = new ParkingSystem(1, 1, 0);
+        Console.WriteLine(p.addCar(1));
+        Console.WriteLine(p.addCar(2));
+        Console.WriteLine(p.addCar(3));
+        Console.WriteLine(p.addCar(1));
+
+        Console.ReadKey();
+    }
+}
+
+class ParkingSystem
+{
+    int big;
+    int medium;
+    int small;
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public bool addCar(int carType)
+    {
+        if (carType == 1)
+        {
+            if (big > 0)
+            {
+                big--;
+                return true;
+            }
+            return false;
+        }
+
+        if (carType == 2)
+        {
+            if (medium > 0)
+            {
+                medium--;
+                return true;
+            }
+            return false;
+        }
+
+        if (carType == 3)
+        {
+            if (small > 0)
+            {
+                small--;
+                return true;
+            }
+            return false;
+        }
+
+        return false;
+    }
+}
+
